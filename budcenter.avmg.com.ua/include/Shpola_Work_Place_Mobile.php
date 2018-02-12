82ce36b88580b29ece957c9e65b51f6f45becdaf
katterina905 <katterina905@gmail.com>
2018-02-12 16:18:17 +0200

    first commit

---
+++ budcenter.avmg.com.ua/include/Shpola_Work_Place_Mobile.php
@@ -0,0 +1,2 @@
+ <h2 class="text-uppercase">Наша адреса</h2>
+<p class="workPlaseText">Черкаська область, м. Шпола,<br>вул. Лебединська, 4-б<br><span>(поряд із автошколою)</span></p><p></p>
---
+++ budcenter.avmg.com.ua/index.php
@@ -0,0 +1,12 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("description", "Будівельні матеріали теперь купувати зручніше ► Завітайте в Новий Будцентр ✔ м. Шпола, вул. Лебединська 4-б ✔ Телефонуйте ☎ (067) 522-24-18");
+$APPLICATION->SetTitle("Будівельний центр АВ метал груп: купити металопрокат, будматеріали: Черкаси, Шпола avmg.com.ua");
+?>
+
+
+
+
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
