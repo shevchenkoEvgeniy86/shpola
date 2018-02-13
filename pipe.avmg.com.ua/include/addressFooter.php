f2f034a40d3c08a56903323b5a2f970f64174db6
shevchenkoEugeniy86 <e.shevchenko@avmg.com.ua>
2018-02-13 08:12:36 +0200

    footer address styles

--- pipe.avmg.com.ua/include/addressFooter.php
+++ pipe.avmg.com.ua/include/addressFooter.php
@@ -1,21 +1,43 @@
 <div class="address-data-footer">
-    <i class="fa fa-map-marker" aria-hidden="true"></i>
-    <address>УкраЇна, Дніпро,<br>
-        вул. Шолом-Алейхема, 5</address>
-    <div class="phone">
-        <i class="fa fa-phone" aria-hidden="true"></i>
-        <div class="box-in-move">
-            <a href="tel:+3(056) 376-79-94">+3(056) 376-79-94</a><br>
+    <div class="center-data-office">
+        <span class="data-box-title">Відділ експортних продажів</span>
+        <span class="center-office-title">Хорунжий Владислав:</span>
+        <div class="center-office-inner">
+            <i class="fa fa-phone" aria-hidden="true"></i>
             <a href="tel:+3(050) 888-89-09">+3(050) 888-89-09</a><br>
-            <a href="tel:+3(067) 485-53-73">+3(067) 485-53-73</a>
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+            <a href="mailto: v.khorunjiy@avmg.com.ua">v.khorunjiy@avmg.com.ua</a>
+        </div>
+        <span class="center-office-title">Ногтєва Оксана:</span>
+        <div class="center-office-inner">
+            <i class="fa fa-phone" aria-hidden="true"></i>
+            <a href="tel:+3(067) 622-84-33">+3(067) 622-84-33</a><br>
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+            <a href="mailto: o.nogteva@avmg.com.ua">o.nogteva@avmg.com.ua</a>
         </div>
     </div>
-    <div class="mail">
-        <i class="fa fa-envelope" aria-hidden="true"></i>
-        <div class="box-in-move">
-            <a href="mailto: office@avmg.com.ua">office@avmg.com.ua</a><br>
-            <a href="mailto: v.khorunjiy@avmg.com.ua">v.khorunjiy@avmg.com.ua</a><br>
-            <a href="mailto: s.kireev@avmg.com.ua">s.kireev@avmg.com.ua</a>
+    <div class="address-data-other">
+        <span class="data-box-title">Загальна інформація</span>
+        <div class="other-data-inner">
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <address>УкраЇна, Дніпро,<br>
+                вул. Шолом-Алейхема, 5</address>
+            <div class="phone">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <div class="box-in-move">
+                    <a href="tel:+3(056) 376-79-94">+3(056) 376-79-94</a><br>
+                    <!--            <a href="tel:+3(050) 888-89-09">+3(050) 888-89-09</a><br>-->
+                    <a href="tel:+3(067) 485-53-73">+3(067) 485-53-73</a>
+                </div>
+            </div>
+            <div class="mail">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <div class="box-in-move">
+                    <a href="mailto: office@avmg.com.ua">office@avmg.com.ua</a><br>
+                    <!--            <a href="mailto: v.khorunjiy@avmg.com.ua">v.khorunjiy@avmg.com.ua</a><br>-->
+                    <a href="mailto: s.kireev@avmg.com.ua">s.kireev@avmg.com.ua</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
